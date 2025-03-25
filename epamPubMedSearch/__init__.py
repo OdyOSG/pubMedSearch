bff04c0303a219ece1a24c373374a8520b78bf30
George Argyriou <george_argyriou@epam.com>
2025-03-25 12:53:35 +0000

    removed llm module file; emptied __init__.py file;

--- epamPubMedSearch/__init__.py
+++
@@ -1,3 +0,0 @@
-import epamPubMedSearch.fn_pubmed as pubmed
-import epamPubMedSearch.fn_llm as llm
-import epamPubMedSearch.spark as spark
--- epamPubMedSearch/spark.py
+++
@@ -1,6 +0,0 @@
-
-
-def getSparkSession(app_name="pubMedSearch"):
-  from pyspark.sql import SparkSession
-  spark.conf.set("spark.sql.execution.arrow.pyspark.enabled", "false")
-  return SparkSession.builder.appName(app_name).getOrCreate()
