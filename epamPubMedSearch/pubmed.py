278af10611fcf0e20bc5e20966ee3b5dabe4f199
George Argyriou <george_argyriou@epam.com>
2025-04-11 13:11:53 +0100

    fixed indent

--- epamPubMedSearch/pubmed.py
+++ epamPubMedSearch/pubmed.py
@@ -840,10 +840,12 @@ def run_pubmed_search(
 
     # Write the Spark DataFrame to the Delta table with schema merging enabled.
     spark_df.write.format("delta") \
-        #.option("overwriteSchema", "true") \
         .mode("append") \
         .saveAsTable(saved_file_name)
 
+
+    #.option("overwriteSchema", "true") \
+    
     # Read back the Delta table into a Spark DataFrame and convert it to a pandas DataFrame.
     result_df = spark.sql("SELECT * FROM {} WHERE phenotype = '{}'".format(saved_file_name, mesh_term))
     
