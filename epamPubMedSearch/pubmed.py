9193c61491dead7b7046d834b76fb36320fdedde
George Argyriou <george_argyriou@epam.com>
2025-04-11 13:09:04 +0100

    removed .option("overwriteSchema", "true") when writing data to spark table

--- epamPubMedSearch/pubmed.py
+++ epamPubMedSearch/pubmed.py
@@ -840,7 +840,7 @@ def run_pubmed_search(
 
     # Write the Spark DataFrame to the Delta table with schema merging enabled.
     spark_df.write.format("delta") \
-        .option("overwriteSchema", "true") \
+        #.option("overwriteSchema", "true") \
         .mode("append") \
         .saveAsTable(saved_file_name)
 
