6d76fab2016a0102b96f560e896127366e607ae9
Gowtham Rao <rao@ohdsi.org>
2025-04-28 16:07:29 -0400

    Update query.py

--- pubmed_search/query.py
+++ pubmed_search/query.py
@@ -1,8 +1,7 @@
 
 """Utility helpers for common query building tasks."""
 
-from searchpubmed.query_builder import build_query, STRATEGY3_OPTS
-
+from searchpubmed.query_builder import build_query, STRATEGY1_OPTS, STRATEGY2_OPTS, STRATEGY3_OPTS, STRATEGY4_OPTS, STRATEGY5_OPTS, STRATEGY6_OPTS, build_query
 
 def rwdSearchTerms(opts=STRATEGY2_OPTS):
     """Return a ready‑made Boolean block for observational RWD studies."""
